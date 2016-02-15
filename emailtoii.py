45193bb3254f85e04328b3f98c881f6e10067301
vit <vityairk@gmail.com>
2016-02-15 21:07:57 +0800

    emailtoii python3 (Ура, работает)

--- emailtoii.py
+++ emailtoii.py
@@ -1,32 +1,32 @@
-#!/usr/bin/env python2
-# -*- coding:utf8 -*-
+#!/usr/bin/env python3
 
 echo="mailbox.15"
 addr="mscript, 1"
 tags="ii/ok"
 
-from ii_functions import *
-
-from email.utils import parsedate,parseaddr
-from email.Header import decode_header
+from email.utils import parsedate, parseaddr
+from email.header import decode_header
 import email.parser as pars
 import os
 import time
-import base64
-import hashlib
 import html2text
+
+from ii_functions import *
+
+check_dirs()
 
 rawletters=os.path.join(cwd, "new/") # new raw messages to parse them
 
-myparser=pars.Parser()
+myparser=pars.BytesParser()
+
 h = html2text.HTML2Text()
 h.ignore_links = True
 
 for filename in os.listdir(rawletters):
 	letterpath=os.path.join(rawletters, filename)
 	
-	plainmessage=open(letterpath).read()
-	message=myparser.parsestr(plainmessage)
+	plainmessage=open(letterpath, "rb").read()
+	message=myparser.parsebytes(plainmessage)
 	
 	msgdate=0
 	sender="nobody"
@@ -34,46 +34,41 @@ for filename in os.listdir(rawletters):
 	subj="..."
 	msg="no message"
 	
-	if(message['date']):
+	if message['date']:
 		parsdate=parsedate(message['date'])
 		msgdate=int(time.mktime(parsdate))
 
-	if(message['from']):
+	if message['from']:
 		sender=parseaddr(message['from'])[1]
-	
-	if(message['to']):
+
+	if message['to']:
 		to=parseaddr(message['to'])[1]
-	
-	if(message['subject']):
-		decodefrag = decode_header(message['subject'])
-		subj_fragments = []
-		for s , enc in decodefrag:
-			if enc:
-				s = unicode(s , enc).encode('utf8','replace')
-			subj_fragments.append(s)
-		subj = ''.join(subj_fragments)
-	
-	if(message.is_multipart()):
+
+	if message['subject']:
+		parts=decode_header(message['subject'])[0]
+		subj=parts[0]
+		charset=parts[1]
+		if charset:
+			subj=subj.decode(charset)
+
+	if message.is_multipart():
 		for part in message.walk():
-			charset=message.get_content_charset()
-			if(not charset):
-				charset='ascii'
+			charset=part.get_content_charset() or 'ascii'
 			payload=part.get_payload(decode=True)
-			if(not payload):
+			if not payload:
 				continue
 			try:
 				payload=payload.decode(charset)
 				msg+="\n"+h.handle(payload).strip().replace("\n\n","\n")
 			except:
-				msg+="can't decode"
-				continue
+				msg+="can't decode\n"
 	else:
 		charset=message.get_content_charset() or "ascii"
 		msg=h.handle(message.get_payload(decode=True).decode(charset)).strip().replace("\n\n", "\n")
-		
+
 	iimsg=tags+"\n"+echo+"\n"+str(msgdate)+"\n"+sender+"\n"+addr+"\n"+to+"\n"+subj+"\n"
-	iimsg+="\n"+msg.encode("utf8")
+	iimsg+="\n"+msg
 
 	msgid=hsh(iimsg)
 	savemsg(msgid, echo, iimsg)
-	os.remove(letterpath)
+	delete(letterpath)
